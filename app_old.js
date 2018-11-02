98de45038949e29abdd49bbf992c0afc889640c0
Daniel <daniloviciv@hotmail.com>
2018-11-02 18:01:43 +0100

    first commit

---
+++ app_old.js
@@ -0,0 +1,19 @@
+const http = require('http');
+
+http.createServer((req, res) => {
+
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+
+        let salida = {
+            nombre: 'Fernando',
+            edad: 32,
+            url: req.url
+        }
+
+        res.write(JSON.stringify(salida));
+        res.end();
+
+    })
+    .listen(8080);
+
+console.log('Listening 8080 port');
